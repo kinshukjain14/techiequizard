bdf8e55c31049fe4b9fde7df7e00f6ae6f88d314
kinshuk.jain14 <kinshuk.jain14@gmail.com>
2022-09-10 15:51:27 +0530

    removed hibernate.cfg.xml

--- src/main/java/com/yash/configuration/QuizAppConfiguration.java
+++ src/main/java/com/yash/configuration/QuizAppConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -39,7 +38,8 @@ public class QuizAppConfiguration {
 	@Bean("sessionFactory")
 	public LocalSessionFactoryBean sessionFactory() {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setConfigLocation(new ClassPathResource("hibernate.cfg.xml"));
+		sessionFactory.setPackagesToScan(new String[] {"com.yash.entities"});
+		sessionFactory.setDataSource(driverManagerDataSource());
 		return sessionFactory;
 	}
 	
